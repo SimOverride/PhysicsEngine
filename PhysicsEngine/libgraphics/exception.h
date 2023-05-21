e1107a522bedbc908c95c9f1f549a12b2ff6242b
XR-stb <2322882096@qq.com>
2023-05-21 16:00:17 +0800

    修复了两个bug 我的环境：win10，vs2022 1. libGraphics/exceptio.c文件拼写错误，应为exception.c 2. RaiseException函数重定义（Windows下应该也是有这么一个函数），我将其改为了PERaiseException

--- PhysicsEngine/libgraphics/exception.h
+++ PhysicsEngine/libgraphics/exception.h
@@ -195,15 +195,15 @@ extern exception ANY;
 extern context_block *exceptionStack;
 
 /*
- * Function: RaiseException
- * Usage: RaiseException(&e, name, value);
+ * Function: PERaiseException
+ * Usage: PERaiseException(&e, name, value);
  * ---------------------------------------
  * This function is called by the raise macro and does the
  * work necessary to raise the exception.  See the exception.c file
  * for details.  Clients do not ordinarily call this directly.
  */
 
-void RaiseException(exception *e, string name, void *value);
+void PERaiseException(exception *e, string name, void *value);
 
 /*
  * Function: HandlerExists
@@ -218,7 +218,7 @@ bool HandlerExists(exception *e);
 
 /* Define the pseudo-functions for raise and try */
 
-#define raise(e) RaiseException(&e, #e, NULL)
+#define raise(e) PERaiseException(&e, #e, NULL)
 
 #define try \
       { \
